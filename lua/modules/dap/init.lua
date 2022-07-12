9fdb50597dce41a1d03e77c9d000a3caa5e28530
ritzier <ritzierwheat@gmail.com>
2022-07-12 12:38:46 +0800

    Push

--- lua/modules/dap/init.lua
+++ lua/modules/dap/init.lua
@@ -1,4 +1,4 @@
-local plugins = { "dap", "dapui" }
+local plugins = { "dap", "dap-install", "dapui" }
 for _, plug in ipairs(plugins) do
     if not pcall(require, plug) then
         print(plug .. " not work")
@@ -6,54 +6,51 @@ for _, plug in ipairs(plugins) do
     end
 end
 
-local dap, dapui = require("dap"), require("dapui")
-
-require("dapui").setup({
-    icons = { expanded = "▾", collapsed = "▸" },
-    mappings = {
-        -- Use a table to apply multiple mappings
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-    },
-    layouts = {
-        {
-            elements = {
-                -- Provide as ID strings or tables with "id" and "size" keys
-                {
-                    id = "scopes",
-                    size = 0.25, -- Can be float or integer > 1
-                },
-                { id = "breakpoints", size = 0.25 },
-                { id = "stacks", size = 0.25 },
-                { id = "watches", size = 0.25 },
-            },
-            size = 40,
-            position = "left",
-        },
-        { elements = { "repl" }, size = 10, position = "bottom" },
-    },
-    floating = {
-        max_height = nil,
-        max_width = nil,
-        mappings = { close = { "q", "<Esc>" } },
-    },
-    windows = { indent = 1 },
+local dap_install = require("dap-install")
+dap_install.setup({
+    installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
 })
 
+local dap_breakpoint = {
+    error = {
+        text = "🟥",
+        texthl = "LspDiagnosticsSignError",
+        linehl = "",
+        numhl = "",
+    },
+    rejected = {
+        text = "",
+        texthl = "LspDiagnosticsSignHint",
+        linehl = "",
+        numhl = "",
+    },
+    stopped = {
+        text = "⭐️",
+        texthl = "LspDiagnosticsSignInformation",
+        linehl = "DiagnosticUnderlineInfo",
+        numhl = "LspDiagnosticsSignInformation",
+    },
+}
+
+vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+
+require("nvim-dap-virtual-text").setup({
+    commented = true,
+})
+
+local dap, dapui = require("dap"), require("dapui")
+dapui.setup({}) -- use default
 dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
 end
-dap.listeners.after.event_terminated["dapui_config"] = function()
+dap.listeners.before.event_terminated["dapui_config"] = function()
     dapui.close()
 end
-dap.listeners.after.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited["dapui_config"] = function()
     dapui.close()
 end
-
-vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
 
 dap.adapters.lldb = {
     type = "executable",
@@ -61,32 +58,4 @@ dap.adapters.lldb = {
     name = "lldb",
 }
 
-dap.adapters.python = {
-    type = "executable",
-    command = "/usr/bin/python3.9",
-    args = { "-m", "debugpy.adapter" },
-}
-dap.configurations.python = {
-    {
-        -- The first three options are required by nvim-dap
-        type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
-        request = "launch",
-        name = "Launch file",
-        -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-        program = "${file}", -- This configuration will launch the current file if used.
-        pythonPath = function()
-            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-            local cwd = vim.fn.getcwd()
-            if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
-                return cwd .. "/venv/bin/python"
-            elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
-                return cwd .. "/.venv/bin/python"
-            else
-                return "/usr/bin/python"
-            end
-        end,
-    },
-}
+require("modules.dap.config.python").setup()
--- requirement.sh
+++ requirement.sh
@@ -1,4 +1,5 @@
-paru -S prettierd google-java-format typescript php composer
-sudo npm install -g fixjson neovim @fsouza/prettierd eslint_d write-good
+#!/bin/bash
+paru -S prettierd google-java-format typescript php composer codelldb
+sudo npm install -g fixjson neovim @fsouza/prettierd eslint_d write-good nginxbeautifier
 pip3 install black djhtml proselint pynvim
 cargo install shellharden
